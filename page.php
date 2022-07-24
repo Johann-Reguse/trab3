611352d0c2e552a23eb5a1c63928a4e24b6febc4
Johann-Reguse <51337281+Johann-Reguse@users.noreply.github.com>
2022-07-24 18:35:09 -0300

    Create page.php

---
+++ page.php
@@ -0,0 +1,13 @@
+
+<?php
+	$dadosJson = file_get_contents('db.json');
+	$dadosJsonDecodificados = json_decode($dadosJson);
+	if(isset($_POST['inserir'])) {
+		echo "Livro enviado";
+		$values = json_encode($_POST);
+		file_put_contents('db.json', $values, FILE_APPEND);
+	}
+	if(isset($_POST['MostrarTudo'])) {
+		print_r($dadosJson);	
+	}
+?>
