aefb14c71f30af1e8b9c684d306c5899ccc81f8e
Johann-Reguse <51337281+Johann-Reguse@users.noreply.github.com>
2022-07-24 21:24:48 -0300

    Update page.php

--- page.php
+++ page.php
@@ -1,32 +1,29 @@
 
 <?php
 	$dadosJson = file_get_contents('db.json');
-	$dadosJsonDecodificados = json_decode($dadosJson);
+	$dadosJsonDecodificados = json_decode($dadosJson, true);
+				
 	if(isset($_POST['inserir'])) {
 		echo "Livro enviado";
 		$values = $_POST;
-		print_r($values);
-		$values = json_encode($_POST);
-		print_r($values);
-		file_put_contents('db.json', $values, FILE_APPEND);
+		$dadosJsonDecodificados["Livro"][] = $values;
+		$fp = fopen('db.json', 'w');
+		fwrite($fp, json_encode($dadosJsonDecodificados));
+		fclose($fp);
 	}
+
 	if(isset($_POST['MostrarTudo'])) {
 		print_r($dadosJson);	
 	}
+	
 	if(isset($_POST['PesquisarLivro'])) {
 		$isbn = $_POST['isbn'];
-		$path = $_POST['path'];
-		print_r($path);
 		foreach ($dadosJsonDecodificados as $key => $value) {
     			if($value == $isbn) {
-        			$msg = 'Encontrado na lista!';
+        			$msg = 'Encontrado na lista!';	
 				print_r($msg);
-				print_r($value);
-				print_r($key);
-				
 			}
 		}
     	}	
 ?>
-<a href='https://trab3.herokuapp.com'>
-	<br>Voltar</a>
+
